e28918e38ce76920c7593c898f437ef4d8421c07
zmjack <jack@zmland.com>
2019-11-13 04:21:05 +0800

    201911130420

---
+++ NMarshal/AutoCharPtr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NWin32
+{
+    public class AutoCharPtr : IDisposable
+    {
+        public StringBuilder Ptr { get; private set; }
+        public int Length => Ptr.Capacity;
+
+        public AutoCharPtr(int length) => Ptr = new StringBuilder(length);
+        public void Dispose() { }
+
+        public string Value => Ptr.ToString();
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static implicit operator StringBuilder(AutoCharPtr @this)
+        {
+            return @this.Ptr;
+        }
+
+    }
+}
