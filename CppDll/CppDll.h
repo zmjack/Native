7a1ebbe73465906ebc1bee65c29444b84777374c
zmjack <jack@zmland.com>
2020-02-08 01:01:00 +0800

    v0.0.5

--- CppDll/CppDll.h
+++ CppDll/CppDll.h
@@ -1,5 +1,6 @@
-#include <Windows.h>
 #pragma once
+#include <Windows.h>
+
 extern "C" __declspec(dllexport) LPWSTR __stdcall CreateString();
 extern "C" __declspec(dllexport) void __stdcall WriteString(LPWSTR lpwstr);
 extern "C" __declspec(dllexport) void __stdcall FreeString(LPWSTR lpwstr);
