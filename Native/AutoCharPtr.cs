819155c4024468fbd6b59464779978f57c3aa4f8
zmjack <jack@zmland.com>
2021-05-29 17:13:28 +0800

    v0.1.1

--- Native/AutoCharPtr.cs
+++ Native/AutoCharPtr.cs
@@ -6,11 +6,12 @@ namespace Native
 {
     public class AutoCharPtr : IDisposable
     {
+        private bool disposedValue;
+
         public StringBuilder Ptr { get; private set; }
         public int Length => Ptr.Capacity;
 
         public AutoCharPtr(int length) => Ptr = new StringBuilder(length);
-        public void Dispose() { }
 
         public string Value => Ptr.ToString();
 
@@ -22,5 +23,33 @@ namespace Native
 
         public override string ToString() => Value;
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~AutoCharPtr()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
--- Native/AutoIntPtr.cs
+++ Native/AutoIntPtr.cs
@@ -14,6 +14,7 @@ namespace Native
         public Type ManagedType { get; private set; }
         public Type ElementType { get; private set; }
         private readonly Array ArrayBuffer;
+        private bool disposedValue;
 
         public AutoIntPtr() : this(0) { }
         public AutoIntPtr(int length)
@@ -32,7 +33,7 @@ namespace Native
             Ptr = Marshal.AllocHGlobal(Length);
         }
 
-        public void Dispose() => Marshal.FreeHGlobal(Ptr);
+        //public void Dispose() => Marshal.FreeHGlobal(Ptr);
 
         public static implicit operator IntPtr(AutoIntPtr<TValue> @this) => @this.Ptr;
 
@@ -113,5 +114,34 @@ namespace Native
 
         public override string ToString() => Value.ToString();
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                Marshal.FreeHGlobal(Ptr);
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~AutoIntPtr()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
